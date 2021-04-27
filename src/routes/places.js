642821eaf242b0d2115c0334bad5600a78a5a576
Krzysztof Czereczon <kczereczon@gmail.com>
2021-04-27 23:25:59 +0200

    Finalizing suggesting places

--- src/routes/places.js
+++ src/routes/places.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { Schema } from "mongoose";
 import { logged } from "../middlewares/logged";
 import { Place } from "../models/Place";
 import { UserTag } from "../models/UserTag";
@@ -87,14 +88,89 @@ placesRouter.get('/around', logged, async (req, res) => {
         let places = await Place.aggregate([
             {
                 $geoNear: {
-                near: {"coordinates": [23.3369131, 50.3487476] },
-                distanceField: "distance",
-                maxDistance: 10000,
-                key: "location",
-                includeLocs: "dist.location",
-                spherical: "true"
+                    near: { "coordinates": [23.3369131, 50.3487476] },
+                    distanceField: "distance",
+                    maxDistance: 10000,
+                    key: "location",
+                    includeLocs: "dist.location",
+                    spherical: "true"
                 }
+            },
+            {$limit: 10}
+        ]);
+
+        res.json(places);
+    } catch (error) {
+        res.json(error.message)
+    }
+
+});
+
+placesRouter.get('/suggested', logged, async (req, res) => {
+
+    // const localization = {
+    //     lat: 50.3487476,
+    //     lon: 23.3369131
+    // }
+
+    try {
+        console.log(req.user._id);
+        let lastTags = await UserTag.aggregate([
+            {
+                $group: {
+                    _id: "$tag",
+                    count: { $sum: 1 }
+                }
+            },
+            { $match: { user: Schema.Types.ObjectId(req.user._id) } },
+            { $sort: { createdAt: -1 } },
+            { $limit: 20 }
+        ]);
+
+        console.log(lastTags);
+
+        let tags = [];
+
+        lastTags.forEach(tag => {
+            if(tag.count > 1) {
+                tags.push(tag._id)
             }
+        });
+
+        let whereTags = [];
+
+        combRep(tags, 3).forEach(item => {
+            whereTags.push({tags: {$all: item}});
+        })
+
+        combRep(tags, 2).forEach(item => {
+            whereTags.push({tags: {$all: item}});
+        })
+
+        combRep(tags, 1).forEach(item => {
+            whereTags.push({tags: {$all: item}});
+        })
+
+        console.log(whereTags);
+
+        let places = await Place.aggregate([
+            {
+                $geoNear: {
+                    near: { "coordinates": [23.3369131, 50.3487476] },
+                    distanceField: "distance",
+                    maxDistance: 200000,
+                    key: "location",
+                    includeLocs: "dist.location",
+                    spherical: "true"
+                }
+            },
+            {
+                $match:
+                {
+                    $or: whereTags
+                }
+            },
+            {$limit: 10}
         ]);
 
         res.json(places);
@@ -105,11 +181,11 @@ placesRouter.get('/around', logged, async (req, res) => {
 });
 
 placesRouter.get('/:id', logged, async (req, res) => {
-    
+
     try {
         let place = await Place.findById(req.params.id);
         place.tags.forEach(async (tag) => {
-            await UserTag.create({user: req.user, tag: tag})
+            await UserTag.create({ user: req.user, tag: tag })
         });
         res.json(place);
     } catch (error) {
@@ -118,3 +194,20 @@ placesRouter.get('/:id', logged, async (req, res) => {
 
 });
 
+
+function combRep(arr, l) {
+    if (l === void 0) l = arr.length; // Length of the combinations
+    var data = Array(l),             // Used to store state
+        results = [];                // Array of results
+    (function f(pos, start) {        // Recursive function
+        if (pos === l) {                // End reached
+            results.push(data.slice());  // Add a copy of data to results
+            return;
+        }
+        for (var i = start; i < arr.length; ++i) {
+            data[pos] = arr[i];          // Update data
+            f(pos + 1, i);                 // Call f recursively
+        }
+    })(0, 0);                        // Start at index 0
+    return results;                  // Return results
+}
